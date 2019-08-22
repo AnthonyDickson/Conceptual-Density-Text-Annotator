73264c1099de489b4be024f62d83c6d97b216465
Anthony Dickson <eight0153@gmail.com>
2019-08-22 14:37:49 +1200

    add endpoint to get: document sections and document sections by section number

--- server/controllers/documents.js
+++ server/controllers/documents.js
@@ -18,3 +18,32 @@ exports.get_document_by_id = (req, res) => {
         }
     )
 };
+
+exports.get_document_sections = (req, res) => {
+    connection.query(
+        `SELECT id, document_id, section_number, title, text
+        FROM document_section 
+        WHERE document_id = ${req.params.documentId}`,
+        (err, rows) => {
+            if (err) throw err;
+
+            res.send({sections: rows});
+        }
+    )
+};
+
+exports.get_document_section = (req, res) => {
+    connection.query(
+        `SELECT id, document_id, section_number, title, text
+        FROM document_section 
+        WHERE 
+            document_id = ${req.params.documentId} 
+                AND
+            section_number = ${req.params.sectionNumber}`,
+        (err, rows) => {
+            if (err) throw err;
+
+            res.send({section: rows[0]});
+        }
+    )
+};
--- server/routes/documents.js
+++ server/routes/documents.js
@@ -5,5 +5,7 @@ var controllers = require('../controllers/documents');
 
 router.get('/', controllers.get_documents);
 router.get('/:documentId', controllers.get_document_by_id);
+router.get('/:documentId/sections', controllers.get_document_sections);
+router.get('/:documentId/sections/:sectionNumber', controllers.get_document_section);
 
 module.exports = router;
