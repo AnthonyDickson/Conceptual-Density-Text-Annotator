39279c089e4b2b9a78dc4d3936917b0ca1de7225
Anthony Dickson <eight0153@gmail.com>
2019-08-23 00:05:40 +1200

    use PORT for heroku

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const documentRouter = require('./routes/documents');
 
 // App Setup
 const app = express();
-const port = process.env.API_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
