d8398834d12ae235895beb4ae7bc0ea59170ec0c
Anthony Dickson <eight0153@gmail.com>
2019-09-08 11:29:29 +1200

    add link to documents from home page

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+
 
 import {Typography} from 'antd';
 
 const Index = () => {
     return <Typography>
-        <Typography.Title>Hello!</Typography.Title>
         <Typography.Paragraph>
             This web app is intended for creating annotations of documents for my COSC480 project,
-            "Quantifying Conceptual Density in Text".
+            "Quantifying Conceptual Density in Text". Documents can be found <Link to="/documents">here</Link>.
         </Typography.Paragraph>
     </Typography>
 };
